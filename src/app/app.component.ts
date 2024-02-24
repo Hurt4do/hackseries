355ba68623fabeff8cb00f59da9f1a80f72efc10
Hurt4do <juansebastian.hurtadop@gmail.com>
2024-02-24 15:30:01 -0500

    feat: Transaction history

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,35 +2,44 @@ import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
 import { ShyftApiService } from './shyft-api.service';
-import { WalletStore } from '@heavy-duty/wallet-adapter';
+import { WalletStore, injectPublicKey } from '@heavy-duty/wallet-adapter';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { computedAsync } from 'ngxtension/computed-async';
+import { MatAnchor } from '@angular/material/button';
 
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HdWalletMultiButtonComponent],
+  imports: [RouterModule, HdWalletMultiButtonComponent, MatAnchor],
   selector: 'agroi-root',
   template: `
-    <header class="py-8">
-      <h1>Hola</h1>
-      <div>
-      <hd-wallet-multi-button></hd-wallet-multi-button>
+    <header class="pb-4 pt-16 relative">
+      <h1 class="text-5xl text-center mb-4">Hola, soy Bob.</h1>
+
+      <div class="flex justify-center absolute top-4 right-4">
+        <hd-wallet-multi-button></hd-wallet-multi-button>
       </div>
-      
-      @if(account()){
-        <div>Conectada</div>
+
+      @if (balance()) {
+        <div
+          class="flex justify-center items-center gap-2 absolute top-4 left-4"
+        >
+          <img src="assets/solana-logo.png" class="w-8 h-8" />
+          <p class="font-bold">{{balance()?.balance }}</p>
+        </div>
       }
     </header>
-    
+
+    <main>
+      <router-outlet></router-outlet>
+    </main>
   `,
 })
 export class AppComponent {
   private readonly _shyftApiService = inject(ShyftApiService);
-  private readonly _walletStore = inject(WalletStore);
-  private readonly _publicKey = toSignal(this._walletStore.publicKey$);
+  private readonly _publicKey = injectPublicKey();
 
-  readonly account = computedAsync(() => this._shyftApiService.getAccount(this._publicKey()?.toBase58()),
-    { requireSync: true },
+  readonly balance = computedAsync(() =>
+    this._shyftApiService.getBalance(this._publicKey()?.toBase58()),
   );
 }
